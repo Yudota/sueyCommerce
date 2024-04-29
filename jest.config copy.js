2abb2424887a5585a629f612fea2978e6cee13fe
Yudota <yuri.fernandes.961@gmail.com>
2024-04-29 10:58:47 -0300

    chore: config jest and docker-compose

---
+++ jest.config copy.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  testRunner: 'jest-jasmine2'
+};
